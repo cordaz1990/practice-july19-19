861f5860b912a1a46dafc936f0299e391f825113
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-13 20:45:27 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,2 +1,13 @@
-print('print')
-print('hello')
+#String is a Sequence
+>>> fruit = 'banana'
+>>> letter = fruit[1]
+a
+>>> letter = fruit[0]
+>>> letter 
+b
+
+index = 0 
+while index < len(fruit):
+    letter = fruit[index]
+    print(letter)
+    index = index + 1 
